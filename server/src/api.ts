26f9f5f178032092bd1c82579971532cb79a8239
sompakorn-lap <sompakorn.lap@student.mahidol.edu>
2025-01-14 21:10:14 +0700

    feat(server): apply GoogleDrive service to api

--- server/src/api.ts
+++ server/src/api.ts
@@ -1,7 +1,21 @@
-import Elysia from "elysia";
+import Elysia, { t } from "elysia";
+import GoogleDrive from "./libs/google/ggdrive";
 
 const api = new Elysia({ prefix: "/api" })
   .get("/", () => "test api")
+  .get("/:fileId", ({ params: { fileId } }) => GoogleDrive.view(fileId))
+  .delete("/:fileId", ({ params: { fileId } }) => GoogleDrive.delete(fileId))
+  .patch("/:fileId", ({ params: { fileId }, body: { testFile } }) => GoogleDrive.update(fileId, testFile), {
+    body: t.Object({
+      testFile: t.File()
+    })
+  })
+  .post("/", ({ body: { fileName, testFile } }) => GoogleDrive.upload(testFile, fileName, "main"), {
+    body: t.Object({
+      fileName: t.String(),
+      testFile: t.File()
+    })
+  })
 ;
 
 export default api;
