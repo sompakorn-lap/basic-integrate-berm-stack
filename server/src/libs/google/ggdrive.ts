61417f47902ba6e4915f2d4c7218e38242ab7a55
sompakorn-lap <sompakorn.lap@student.mahidol.edu>
2025-01-14 21:09:36 +0700

    feat(server): add ggauth, ggdrive and GoogleDrive service

---
+++ server/src/libs/google/ggdrive.ts
@@ -0,0 +1,60 @@
+import { google } from "googleapis";
+import ggauth from "./ggauth";
+import { Readable } from "stream";
+
+const ggdrive = google.drive({
+  version: "v3",
+  auth: ggauth,
+});
+
+const folders = JSON.parse(process.env.GGDRIVE_FOLDERS as string);
+
+class GoogleDrive {
+
+  static async upload(file: File, fileName: string, folder: string) {
+    const buffer = Buffer.from(await file.arrayBuffer());
+    const stream = Readable.from(buffer);
+    
+    const { data } = await ggdrive.files.create({
+      requestBody: {
+        name: fileName,
+        parents: [folders[folder]]
+      },
+      media: {
+        body: stream,
+        mimeType: file.type
+      }
+    });
+
+    return data.id;
+  }
+  
+  static async view(fileId: string) {
+    const { data } = await ggdrive.files.get(
+      { fileId, alt: "media", fields: "mimeType" },
+      { responseType: "stream" }
+    );
+
+    return data;
+  }
+
+  static async update(fileId: string, file: File) {
+    const buffer = Buffer.from(await file.arrayBuffer());
+    const stream = Readable.from(buffer);
+
+    await ggdrive.files.update({
+      fileId,
+      media: {
+        body: stream,
+        mimeType: file.type
+      }
+    });
+  }
+
+  static async delete(fileId: string){
+    await ggdrive.files.delete({ fileId });
+  }
+  
+}
+
+export default GoogleDrive;
